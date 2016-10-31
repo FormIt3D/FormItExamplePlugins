20aa1bee7f4d46661e05c6be9280f7bfb6bc1958
Jeff Hauswirth <jeff.hauswirth@autodesk.com>
2016-10-31 13:36:50 -0600

    Making Toolbar Plugin (more) generic

--- Example1/jeffsplugin.js
+++
@@ -1,6 +0,0 @@
-JeffsPlugin = {};
-JeffsPlugin.Submit = function()
-{
-    console.log("It WORKED!!");
-}
-FormIt.Commands.RegisterJSCommand("JeffsPlugin.Submit");
---
+++ Example1/toolbarplugin.js
@@ -0,0 +1,6 @@
+ToolbarPlugin = {};
+ToolbarPlugin.Button = function()
+{
+    console.log("Toolbar Plugin button clicked!!");
+}
+FormIt.Commands.RegisterJSCommand("ToolbarPlugin.Button");
