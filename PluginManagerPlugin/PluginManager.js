f3a61058b46ab5dbbda2d0c40d318264770647d7
jhauswirth <jeff_hauswirth@pobox.com>
2017-06-21 18:53:18 -0600

    Swapping in PM.

--- PluginManagerPlugin/PluginManager.js
+++ PluginManagerPlugin/PluginManager.js
@@ -2,6 +2,29 @@ PluginManager = {};
 
 PluginManager.InstalledPlugins = [];
 
+PluginManager.createHeader = function() {
+    var headerDiv = document.createElement('div');
+    headerDiv.id = 'header';
+    headerDiv.className = 'header';
+    headerDiv.innerHTML = 'Plugin Manager';
+    window.document.body.appendChild(headerDiv);
+
+    var linkRepoContainer = document.createElement('div');
+    linkRepoContainer.style = "display: block";
+    headerDiv.appendChild(linkRepoContainer);
+
+    var linkRepoInput = document.createElement('INPUT');
+    linkRepoInput.setAttribute("type", "text");
+    linkRepoInput.setAttribute("placeholder", "Paste new repo URL here...");
+    linkRepoInput.className = 'linkRepoInput';
+    linkRepoContainer.appendChild(linkRepoInput);
+
+    var linkRepoButton = document.createElement('button');
+    linkRepoButton.setAttribute("type", "input");
+    linkRepoButton.className = 'linkRepoButton';
+    linkRepoContainer.appendChild(linkRepoButton);
+}
+
 PluginManager.RemovePluginFromInstalled = function(pluginLocation)
 {
     var newInstalledList = [];
@@ -15,16 +38,28 @@ PluginManager.RemovePluginFromInstalled = function(pluginLocation)
     PluginManager.InstalledPlugins = newInstalledList;
 }
 
-PluginManager.parentAccordionFunction = function() {
-    var exampleName = "<div id='repoName'>Example Plugins</div>";
-    $(".accordionParent").append(exampleName);
-    $(".accordionParent").append($(".accordion"));
-    $(".accordionParent").accordion("refresh");
+PluginManager.MakePluginDivs = function()
+{
+    var plugins = JSON.parse(this.responseText);
+    if (plugins)
+    {
+        plugins.forEach(function(element) {
+                // Now add the plugins.
+                //var pluginManifest = this.pluginSiteURL + "/" + element + "/manifest.json";
+                var request = new XMLHttpRequest();
+                request.addEventListener("load", PluginManager.MakePluginDiv);
+                request.pluginSiteURL = this.pluginSiteURL;
+                request.parentElemDiv = this.parentElemDiv;
+                request.PluginURL = this.pluginSiteURL + "/" + element;
+                request.open("GET", this.pluginSiteURL + "/" + element + "/manifest.json");
+                request.send();
+        }, this);
+    }
 }
 
-PluginManager.GetPluginManifest = function()
+PluginManager.MakePluginDiv = function(plugin)
 {
-    console.log("---> PluginManager.GetPluginManifest");
+    console.log("---> PluginManager.MakePluginDiv");
     //TODO (herrj): Check if this plugin is in installed plugin list and skip if it is not.
     var pluginData = JSON.parse(this.responseText);
     var pluginName = pluginData["PluginName"];
@@ -35,82 +70,128 @@ PluginManager.GetPluginManifest = function()
     //NOTE: "CustomDescription": *.html
     var pluginCustomDescription = pluginData["CustomDescription"];
     console.log("Custom Description: " + pluginCustomDescription);
-    var descriptionContentHTML = "";
-    // plugInInstalled returns a boolean value for whether the current plugin is installed.
-    var plugInInstalled = PluginManager.InstalledPlugins.indexOf(pluginLocation) > -1;
-    // plugInChecked returns a "checked" value to insert in the checkbox div, if the plugin is installed at initialization time
-    var plugInChecked;
-    if (plugInInstalled) {
-        plugInChecked = "checked";
-    }
 
-    var pluginCheckboxName = pluginName.replace(/\s/g,'') + "Checkbox";
-    var plugInCheckBox = "<div id='checkbox' style='float: right; display: inline;'><input type='checkbox' id='" + pluginCheckboxName + "' name='plugInCheckBox' value='Installed' " + plugInChecked + "></input></div>";
-    var pluginNameHTML = "<div><div id='pluginName''>" + pluginName + "</div>" + plugInCheckBox + "</div>";
+    var pluginContainerDiv = document.createElement('div');
+    pluginContainerDiv.id = pluginName.replace(/\s/g,'') + "Container";
+    this.parentElemDiv.appendChild(pluginContainerDiv);
+        
+    var pluginNameDiv = document.createElement('div');
+    pluginNameDiv.id = 'pluginName' + pluginName.replace(/\s/g,'');
+    pluginNameDiv.className = 'pluginName';
+    pluginNameDiv.innerHTML = pluginName;
+    pluginContainerDiv.appendChild(pluginNameDiv);
+    pluginContainerDiv.className = 'pluginContainer';
+    //this.parentElemDiv.appendChild(pluginNameDiv);
+
+
+    var pluginDescriptionDiv = document.createElement('div');
+    pluginDescriptionDiv.id = pluginName.replace(/\s/g,'') + "Description";
+    pluginDescriptionDiv.className = 'pluginDescription';
+    pluginDescriptionDiv.style = 'clear: both;';
+    pluginContainerDiv.appendChild(pluginDescriptionDiv);
+    // start accordion collapsed
+    pluginDescriptionDiv.style.display = "none";
+
+    pluginNameDiv.onclick = function(e) 
+        {
+            pluginDescriptionDiv.classList.toggle("active");
+            if (pluginDescriptionDiv.style.display === "block") 
+                {
+                pluginDescriptionDiv.style.display = "none";
+                } else {
+                pluginDescriptionDiv.style.display = "block";
+                }
+            e.stopPropagation();
+        }
+
+    pluginDescriptionDiv.onclick = function(e)
+        {
+            e.stopPropagation();
+        }
+
+
+    // Add checkbox input
+    var checkboxElemDiv = document.createElement('div');
+    checkboxElemDiv.id = pluginName.replace(/\s/g,'') + "Checkbox";
+    checkboxElemDiv.className = "checkboxDiv";
+    //checkboxElemDiv.innerHTML = pluginName;
+
+    pluginNameDiv.appendChild(checkboxElemDiv);
+    var checkboxElem = document.createElement("input");
+    checkboxElem.id = pluginName.replace(/\s/g,'') + "Checkbox";
+    checkboxElem.type = 'checkbox';
+    checkboxElem.name = 'plugInCheckBox';
+    checkboxElem.checked = PluginManager.InstalledPlugins.indexOf(pluginLocation) > -1;
+    checkboxElemDiv.onclick = function(e)
+    {
+        if (PluginManager.InstalledPlugins.indexOf(pluginLocation) > -1)
+        {
+            FormItInterface.CallMethod("FormIt.UninstallPlugin",  JSON.stringify(pluginLocation));
+            PluginManager.RemovePluginFromInstalled(pluginLocation);
+        }
+        else
+        {
+            FormItInterface.CallMethod("FormIt.InstallPlugin", JSON.stringify(pluginLocation));
+            PluginManager.InstalledPlugins.push(pluginLocation);
+        }
+        e.stopPropagation();
+    }
+    checkboxElemDiv.appendChild(checkboxElem);
+
     //Thing will diverge here with custom description...
     if(pluginCustomDescription != undefined)
     {
-      //create an iframe to encapsulate the plugin custom description html.
-      descriptionContentHTML = "<div><iframe src='" + pluginLocation + "/" + pluginCustomDescription +
-        "'></iframe></div>";
+        pluginDescriptionDiv.appendChild(document.createTextNode(pluginDescription));
     }
     else
     {
         if(pluginDescription == undefined)
         {
             pluginDescription = "Description not provided for this plugin.";
+            pluginDescriptionDiv.appendChild(document.createTextNode(pluginDescription));
         }
-
-        descriptionContentHTML = "<div style='clear: both'>" + pluginDescription + "</div>";
     }
-
-        var installFunctionCB = function() {
-        FormItInterface.CallMethod("FormIt.InstallPlugin", JSON.stringify(pluginLocation));
-        PluginManager.InstalledPlugins.push(pluginLocation);
-        console.log("PluginManager.InstalledPlugins: " + JSON.stringify(PluginManager.InstalledPlugins));
-        console.log("Attempted to install: " + pluginLocation);
-        this.value = "Uninstall";
-        this.onclick = uninstallFunctionCB;
-        document.getElementById(pluginCheckboxName).checked = true;
-    };
-
-    var uninstallFunctionCB = function() {
-        FormItInterface.CallMethod("FormIt.UninstallPlugin",  JSON.stringify(pluginLocation));
-        PluginManager.RemovePluginFromInstalled(pluginLocation);
-        console.log("PluginManager.InstalledPlugins: " + JSON.stringify(PluginManager.InstalledPlugins));
-        console.log("Attempted to uninstall: " + pluginLocation);
-        this.value = "Install";
-        this.onclick = installFunctionCB;
-        document.getElementById(pluginCheckboxName).checked = false;
-    };
-
-    var pluginCheckboxFunc = uninstallFunctionCB;
-
-    if (plugInInstalled)
-    {
-        pluginCheckboxFunc = uninstallFunctionCB;
-    }
-    else
-    {       
-        pluginCheckboxFunc = installFunctionCB;
-    }
-    $(".accordion").append(pluginNameHTML);
-    $(".accordion").append(descriptionContentHTML);
-    $(".accordion").accordion("refresh");
-   
-    var plugInCheckbox = document.getElementById(pluginCheckboxName);
-    plugInCheckbox.onclick = pluginCheckboxFunc;
-    $('.accordion input[type="checkbox"]').click(function(e) {
-    e.stopPropagation();
-});
-    console.log(pluginName + " was added to accordion.");
 }
 
+PluginManager.AddPluginRepo = function(name, pluginSiteURL)
+{   
+    var repoElemDiv = document.createElement('div');
+    repoElemDiv.id = name.replace(/\s/g,'');
+    repoElemDiv.className = "repoName";
+    repoElemDiv.innerHTML = name;
+    window.document.body.appendChild(repoElemDiv);
 
+    var repoContainerDiv = document.createElement('div');
+    repoContainerDiv.id = name.replace(/\s/g,'') + "Container";
+    repoContainerDiv.className = "repoContainer";
+    repoElemDiv.appendChild(repoContainerDiv);
+    // start accordion collapsed
+    repoElemDiv.firstElementChild.style.display = "none";
+    repoElemDiv.onclick = function() 
+    {
+        repoContainerDiv.classList.toggle("active");
+        var firstChild = repoElemDiv.firstElementChild;
+        if (firstChild.style.display === "block") 
+            {
+            firstChild.style.display = "none";
+            } else {
+            firstChild.style.display = "block";
+        }
+    }
 
-PluginManager.BuildPluginInformation = function()
+    // Now add the plugins.
+    var pluginsManifest = pluginSiteURL + "/plugins.json";
+    var request = new XMLHttpRequest();
+    request.addEventListener("load", PluginManager.MakePluginDivs);
+    request.pluginSiteURL = pluginSiteURL;
+    request.parentElemDiv = repoContainerDiv;
+    request.open("GET", pluginsManifest);
+    request.send();
+}
+
+PluginManager.MakePluginRepoDivs = function()
 {
-    console.log("---> PluginManager.BuildPluginInformation");
+    console.log("---> PluginManager.MakePluginRepoDivs");
 
     //TODO (hauswij): Figure out how to get the full URL of the plugins. This will be needed for installed comparisons.
     //Ex. FormIt will return: http://localhost:8000/PluginManager for the installed PluginManager. Need to figure out
@@ -121,22 +202,12 @@ PluginManager.BuildPluginInformation = function()
     // I'm hard-coding this bit for now.
     var originURL = document.URL;
     originURL = originURL.replace('PluginManagerPlugin\/PluginManager.html', '');
-    console.log("Doc URL: " + document.URL);
-    console.log("Origin URL: " + originURL);
     var pluginArray = JSON.parse(this.responseText);
-    console.log("pluginArray: " + JSON.stringify(pluginArray));
-    PluginManager.parentAccordionFunction();
-    for(var i=0; i < pluginArray["Plugins"].length; i++)
+    for(var i=0; i < pluginArray.length; i++)
     {
-        var pluginURL = originURL + pluginArray["Plugins"][i];
-        //console.log("Building plugin request for: " + pluginURL);
-        var manifestURL = pluginURL + "/manifest.json";
-        //console.log("manifestURL: " + manifestURL);
-        var request = new XMLHttpRequest();
-        request.addEventListener("load", PluginManager.GetPluginManifest);
-        request.PluginURL = pluginURL;
-        request.open("GET", manifestURL);
-        request.send();
+        var name = pluginArray[i].Name;
+        var pluginSiteURL = pluginArray[i].URL;
+        PluginManager.AddPluginRepo(name, pluginSiteURL);
     }
 }
 
@@ -145,6 +216,7 @@ PluginManager.CreatePlugins = function()
     console.log("---> PluginManager.CreatePlugins");
     //Clear the body to reconstruct the plugin UI.
     //document.body.innerHTML = "";
+    PluginManager.createHeader();
     if (true)
     {
         //Start by getting internal plugins and adding them to the panel.
@@ -153,14 +225,18 @@ PluginManager.CreatePlugins = function()
             function(installedPlugins)
             {
                 PluginManager.InstalledPlugins = eval(installedPlugins);
+                if (!PluginManager.InstalledPlugins)
+                {
+                    PluginManager.InstalledPlugins = [];
+                }
                 console.log("PluginManager.InstalledPlugins: " + JSON.stringify(PluginManager.InstalledPlugins));
 
                 //Get the list of plugins from the top level manifest
                 //Keep things synchronous. Use callback method to spin off creation of plugins
-                console.log("************** Requesting plugins.json to call PluginManager.BuildPluginInformation.");
+                console.log("************** Requesting plugins.json to call PluginManager.MakePluginRepoDivs.");
                 var request = new XMLHttpRequest();
-                request.addEventListener("load", PluginManager.BuildPluginInformation);
-                request.open("GET", "plugins.json");
+                request.addEventListener("load", PluginManager.MakePluginRepoDivs);
+                request.open("GET", "pluginsites.json");
                 request.send();
             });                
     }
