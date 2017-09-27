f25f3eb50c8d1aaa95b58298f70ef6a273e0363c
Jeff Hauswirth <jeff_hauswirth@pobox.com>
2017-09-27 10:51:31 -0600

    Restoring FormIt.Commands.RegisterJSCommand

--- Toolbar1/toolbarplugin.js
+++ Toolbar1/toolbarplugin.js
@@ -3,4 +3,4 @@ ToolbarPlugin.Button = function()
 {
     console.log("Toolbar Plugin button clicked!!");
 }
-//FormIt.Commands.RegisterJSCommand("ToolbarPlugin.Button");
+FormIt.Commands.RegisterJSCommand("ToolbarPlugin.Button");
