ac43e90d745aecc7277068065873a42a9a0bf6a8
Jeff Hauswirth <jeff.hauswirth@autodesk.com>
2017-12-12 13:15:17 -0700

    Making Quantum recharge plugin
    Making Quantum recharge plugin

---
+++ ObjectAttributes2/wsmattribute.js
@@ -0,0 +1,39 @@
+if (typeof FormItExamplePlugins == 'undefined')
+{
+    FormItExamplePlugins = {};
+}
+
+if (typeof FormItExamplePlugins.ObjectAttributes2 == 'undefined')
+{
+    FormItExamplePlugins.ObjectAttributes2 = {};
+}
+
+// Submit runs from the HTML page.
+FormItExamplePlugins.ObjectAttributes2.Submit = function()
+{
+    var args = {
+        "key": document.a.key.value,
+        "value": document.a.buildingType.value
+    }
+    debugger;
+    FormItInterface.ConsoleLog("Create Attribute");
+    FormItInterface.ConsoleLog("args: " + JSON.stringify(args));
+    FormItInterface.CallMethod("FormItExamplePlugins.ObjectAttributes2.ApplyWSMAttriblute", args);
+}
+
+// FormItExamplePluginsApplyWSMAttriblute runs from the FormIt App side.
+FormItExamplePlugins.ObjectAttributes2.ApplyWSMAttriblute = function(args)
+{
+    if (FormIt.Selection.HasSelections())
+    {
+        FormIt.UndoManagement.BeginState();
+        var selectedObjs = FormIt.Selection.GetSelections();
+        selectedObjs.forEach(function(obj) {
+            console.log("obj: " + JSON.stringify(obj));
+            var WSMObj = obj.ids[obj.ids.length - 1];
+            console.log("WSMObj: " + JSON.stringify(WSMObj));
+            WSM.APICreateStringAttribute(WSMObj.History, args.key, args.value, WSMObj.Object);
+        }, this);
+        FormIt.UndoManagement.EndState("FormItExamplePluginsApplyWSMAttriblute");
+    }
+}
