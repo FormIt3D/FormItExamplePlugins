9e2c715934a1004cfba0d70e05a7cf726b923618
Jeff Hauswirth <jeff_hauswirth@pobox.com>
2018-05-24 16:54:23 -0600

    Create panel from toolbar button
    The button will only create one instance of the panel when clicking the toolbar button multiple times.

--- Toolbar1/toolbarplugin.js
+++ Toolbar1/toolbarplugin.js
@@ -1,6 +1,22 @@
 ToolbarPlugin = {};
+
 ToolbarPlugin.Button = function()
 {
     console.log("Toolbar Plugin button clicked!!");
 }
 FormIt.Commands.RegisterJSCommand("ToolbarPlugin.Button");
+
+ToolbarPlugin.HelloBlockPanelCreated = false;
+ToolbarPlugin.CreatePanel = function()
+{
+    if (!ToolbarPlugin.HelloBlockPanelCreated)
+    {
+        FormIt.UI.CreatePanel("Only One Panel", "https://formit3d.github.io/FormItExamplePlugins/Toolbar1/panel.html");
+    }
+    else
+    {
+        console.log("Panel already created!!");
+    }
+    ToolbarPlugin.HelloBlockPanelCreated = true;
+}
+FormIt.Commands.RegisterJSCommand("ToolbarPlugin.CreatePanel");
