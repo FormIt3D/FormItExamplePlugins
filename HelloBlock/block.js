00c1c75456f9dd1be951314a97c287cb3e1a83c0
Jeff Hauswirth <jeff.hauswirth@autodesk.com>
2019-02-19 15:21:53 -0700

    Take into account current unit settings.

--- HelloBlock/block.js
+++ HelloBlock/block.js
@@ -1,9 +1,19 @@
+
+function currentUnits(length)
+{
+    var kMetricFactor = 100.0 / (2.54 * 12.0);
+    if (FormIt.Model.GetUnitType() === FormIt.UnitType.kMetric)
+        return length * kMetricFactor;
+
+    return length;
+}
+
 // CreateBlock runs from Pyramid.
 function CreateBlock(args)
 {
     var histID = FormIt.GroupEdit.GetEditingHistoryID();
     var pt1 = WSM.Geom.Point3d(0.0, 0.0, 0.0);
-    var pt2 = WSM.Geom.Point3d(args.w, args.l, args.h);
+    var pt2 = WSM.Geom.Point3d(currentUnits(args.w), currentUnits(args.l), currentUnits(args.h));
     var blockID = WSM.APICreateBlock(histID, pt1, pt2);
 }
 
