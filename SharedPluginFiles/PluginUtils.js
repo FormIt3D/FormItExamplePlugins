e1bcdc945cf74728175b638951c278fb602036d3
Jeff Hauswirth <jeff.hauswirth@autodesk.com>
2019-02-27 15:27:55 -0700

    Adding FormIt.PluginUtils.HandleEnter.

--- SharedPluginFiles/PluginUtils.js
+++ SharedPluginFiles/PluginUtils.js
@@ -1,5 +1,11 @@
+if (typeof FormIt === 'undefined')
+{
+    FormIt = {};
+}
+
 FormIt.PluginUtils = {};
 
+// Function to conver the given length into the current FormIt units.
 FormIt.PluginUtils.currentUnits = function(length)
 {
     var kMetricFactor = 100.0 / (2.54 * 12.0);
@@ -7,4 +13,21 @@ FormIt.PluginUtils.currentUnits = function(length)
         return length * kMetricFactor;
 
     return length;
+}
+
+// Function to prevent HTML page from reloading when hitting ENTER.
+FormIt.PluginUtils.HandleEnter = function(formId, buttonId)
+{
+    var formElement = document.getElementById(formId);
+    formElement.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            document.getElementById(buttonId).click();
+            event.preventDefault();
+        }
+    });
+    formElement.addEventListener("keydown", function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+        }
+    });
 }
