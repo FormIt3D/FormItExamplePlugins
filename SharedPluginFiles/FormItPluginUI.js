e9a12389ec889336cd9a988a022598ba9cecf53a
Josh Goldstein <josh.goldstein@autodesk.com>
2020-05-06 13:54:01 -0600

    Adding footer support to core plugin files

--- SharedPluginFiles/FormItPluginUI.js
+++ SharedPluginFiles/FormItPluginUI.js
@@ -62,3 +62,46 @@ FormIt.PluginUI.createTextInputModule = function(moduleParent, moduleLabelText, 
 
     return textInputModuleContainer;
 }
+
+// create a typical FormIt Plugin footer with information about the plugin
+FormIt.PluginUI.createFooter = function() {
+    var footerContainer = document.createElement('div');
+    footerContainer.id = 'footerContainer';
+    footerContainer.className = 'footerContainer';
+    window.document.body.appendChild(footerContainer);
+
+    var footerDiv = document.createElement('div');
+    footerDiv.id = 'footerDiv';
+    footerDiv.className = 'footerDiv';
+    footerContainer.appendChild(footerDiv);
+
+    var footerDescriptionText = document.createTextNode("Powered by FormIt JavaScript plugins");
+    footerDiv.appendChild(footerDescriptionText);
+
+    var footerDivUL = document.createElement('ul');
+    footerDiv.appendChild(footerDivUL);
+
+    var footerLearnAboutPluginsLI = document.createElement('li');
+    var footerLearnAboutPluginsLink = document.createElement('a');
+    var footerLearnAboutPluginsText = document.createTextNode("Learn about plugins");
+    footerLearnAboutPluginsLink.appendChild(footerLearnAboutPluginsText);
+    footerLearnAboutPluginsLink.setAttribute("href", "javascript:void(0);");
+    footerDivUL.appendChild(footerLearnAboutPluginsLI);
+    footerLearnAboutPluginsLI.appendChild(footerLearnAboutPluginsLink);
+
+    footerLearnAboutPluginsLink.onclick = function() {
+        FormItInterface.CallMethod('FormItExamplePlugins.PluginManager.OpenAboutPluginsURL', "");
+    }
+
+    var footerLearnToBuildLI = document.createElement('li');
+    var footerLearnToBuildLink = document.createElement('a');
+    var footerLearnToBuildText = document.createTextNode("Build your own");
+    footerLearnToBuildLink.appendChild(footerLearnToBuildText);
+    footerLearnToBuildLink.setAttribute("href", "javascript:void(0);");
+    footerDivUL.appendChild(footerLearnToBuildLI);
+    footerLearnToBuildLI.appendChild(footerLearnToBuildLink);
+
+    footerLearnToBuildLink.onclick = function() {
+        FormItInterface.CallMethod('FormItExamplePlugins.PluginManager.OpenBuildPluginsURL', "");
+    }
+}
