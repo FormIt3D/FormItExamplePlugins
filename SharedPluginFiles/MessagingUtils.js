5fe904640510180e2bb910e3116c7b066344caf9
schaefm <matt.schaeffer@autodesk.com>
2020-05-18 13:42:22 -0600

    Adds some functionality to test Windows support

--- SharedPluginFiles/MessagingUtils.js
+++ SharedPluginFiles/MessagingUtils.js
@@ -6,10 +6,25 @@ if (typeof FormIt === 'undefined')
 FormIt.MessagingUtils = {};
 
 FormIt.MessagingUtils.dipatchMessage = (channel, message) => {
-    window.parent.postMessage({channel, message}, "*");
+    //support for Windows
+    if(FormItInterface.EmitMessage){
+        FormItInterface.EmitMessage(JSON.stringify(message))
+    }
+    //support for Web
+    else{
+        window.parent.postMessage({channel, message}, "*");
+    }
 }
 
 FormIt.MessagingUtils.listenForMessage = (handler) => {
+    //support fow Windows
+    window.FormIt.MessagingUtils.ReceiveMessage = (message) => {
+        if (message){
+            handler(message);
+        }
+    }
+
+    //support for Web
     window.addEventListener('message', (message) => {
         if (message.data){
             handler(message.data);
