1fc7a90cb490f2431686dd79c15908ef7c90ec1d
jdeangoldstein <josh.goldstein@autodesk.com>
2020-06-25 08:50:25 -0600

    FORMIT-10713: Plugins: update PluginUtils18_0 with new unit support (#16)
    - Updating PluginUtils_18 with new unit support
    - Updating plugins to look at relative path for shared scripts

--- SharedPluginFiles/PluginUtils18_0.js
+++ SharedPluginFiles/PluginUtils18_0.js
@@ -5,14 +5,34 @@ if (typeof FormIt === 'undefined')
 
 FormIt.PluginUtils = {};
 
-// Function to conver the given length into the current FormIt units.
+// convert the given length into the current FormIt units
+// this function only works on the FormIt side
 FormIt.PluginUtils.currentUnits = function(length)
 {
-    var kMetricFactor = 100.0 / (2.54 * 12.0);
-    if (FormIt.Model.GetUnitTypeCurrent() === FormIt.UnitType.kMetric)
-        return length * kMetricFactor;
+    var currentUnitType = FormIt.Model.GetUnitTypeCurrent();
 
-    return length;
+    // the default FormIt unit is 1 foot,
+    // so calculate all other units relative to that
+    var kImperialInchFactor = 1/12;
+    var kMetricMeterFactor = 100.0 / (2.54 * 12.0);
+    var kMetricCentimeterFactor = kMetricMeterFactor / 100;
+    var kMetricMillimeterFactor = kMetricMeterFactor / 1000;
+
+    switch (currentUnitType)
+    {
+        case FormIt.UnitType.kImperialInches:
+            return  length * kImperialInchFactor;
+        case FormIt.UnitType.kMetricMeter:
+            return  length * kMetricMeterFactor;
+        case FormIt.UnitType.kMetricCentimeter:
+            return  length * kMetricCentimeterFactor;
+        case FormIt.UnitType.kMetricMillimeter:
+            return  length * kMetricMillimeterFactor;
+        // if it's none of these, we're in feet, which is the default
+        // so return the length unchanged
+        default:
+            return length;
+    }
 }
 
 // Function to prevent HTML page from reloading when hitting ENTER.
