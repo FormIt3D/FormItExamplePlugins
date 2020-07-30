ad322793dfd27442196636a68b7a008110863f17
Nathaniel Cesario <nathaniel.cesario@gmail.com>
2020-07-30 17:44:13 -0600

    Minor refactor

--- HelloBlockAsync/block.js
+++ HelloBlockAsync/block.js
@@ -1,4 +1,5 @@
 import { FormIt, WSM } from '/SharedPluginFiles/formit_js_async_polyfill.mod.js';
+import { FormItPluginUtils } from '/SharedPluginFiles/FormItPluginUtils.mod.js';
 
 export class CreateBlockButton extends HTMLElement {
     constructor() {
@@ -6,25 +7,29 @@ export class CreateBlockButton extends HTMLElement {
         this.shadow = this.attachShadow({mode: 'open'});
         this.shadow.innerHTML = `
         <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
-        <div>
-            <input id="width" type=number value="12.0">Width</input><br/>
-            <input id="height" type=number value="12.0">Length</input><br/>
-            <input id="length" type=number value="12.0">Height</input><br/>
-            <button>Create Block</button>
-        </div>
+        <button>Create Block</button>
         `;
 
         this.shadow.querySelector('button').addEventListener('click', this.createBlock.bind(this));
+        this.histID = WSM.INVALID_ID;
+
     }
 
     async createBlock() {
-        const w = parseFloat(this.shadow.querySelector('#width').value);
-        const h = parseFloat(this.shadow.querySelector('#height').value);
-        const l = parseFloat(this.shadow.querySelector('#length').value);
-        const histID = FormIt.GroupEdit.GetEditingHistoryID();
+        const w = this.width(), h = this.height(), l = this.length();
         const pt1 = WSM.Geom.Point3d(0,0,0);
-        const pt2 = WSM.Geom.Point3d(w, h, l);
-        WSM.APICreateBlock(await histID, await pt1, await pt2);
+        const pt2 = WSM.Geom.Point3d(await w, await h, await l);
+        this.histID = await FormIt.GroupEdit.GetEditingHistoryID();
+        this.blockID = await WSM.APICreateBlock(this.histID, await pt1, await pt2);
+    }
+
+    async width() { return FormItPluginUtils.currentUnits(this.numFromElem("width")); }
+    async height() { return FormItPluginUtils.currentUnits(this.numFromElem("height")); }
+    async length() { return FormItPluginUtils.currentUnits(this.numFromElem("length")); }
+
+    numFromElem(id) {
+        const elem = document.querySelector(this.getAttribute(id));
+        return (elem) ? parseFloat(elem.value) : 0;
     }
 }
 
