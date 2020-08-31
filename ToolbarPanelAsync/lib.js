6bf6845ea6332cc17fd53082d2a6466d0ceb1846
nces <nathaniel.cesario@gmail.com>
2020-08-31 17:13:16 -0600

    Toolbar + Panel example using async APIs (#21)

---
+++ ToolbarPanelAsync/lib.js
@@ -0,0 +1,21 @@
+import { FormIt, WSM } from '../SharedPluginFiles/FormIt.mod.js';
+
+class MyFormItLib {
+    constructor() {
+        this._version = '1.0.0';
+    }
+
+    async alertVersions() {
+        const version = await FormIt.GetVersion();
+        alert(`<p>FormIt version = ${JSON.stringify(version)}</p><p>Plugin version = ${this._version}</p>`);
+    }
+
+    async createBlock(data) {
+        const pt1 = WSM.Geom.Point3d(-6, -6, 0);
+        const pt2 = WSM.Geom.Point3d(6, 6, 12);
+        const history = FormIt.GroupEdit.GetEditingHistoryID();
+        WSM.APICreateBlock(await history, await pt1, await pt2);
+    };
+}
+
+export { MyFormItLib };
